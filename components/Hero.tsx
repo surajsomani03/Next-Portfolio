ea9ed90ee73d1b84eb612e38f2340b5c166b4eb7
Suraj Somani <surajsomani2002@gmail.com>
2024-05-23 15:02:42 +0530

    Bento grid section completed

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -31,7 +31,7 @@ const Hero = () => {
             words="Transforming Concepts into Seamless User Experiences"
           />
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi, I&apos;m Suraj, a Next.js Developer based in Croatia
+            Hi, I&apos;m Suraj, a Next.js Developer based in India
           </p>
           <a href="#about">
             <MagicButton

594181ea3bc30661c8cdb87df7bb783880b0f789
Suraj Somani <surajsomani2002@gmail.com>
2024-05-31 20:42:41 +0530

    sentry added successfully

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -52,8 +52,8 @@ export const BentoGridItem = ({
   titleClassName?: string;
   spareImg?: string;
 }) => {
-  const leftLists = ["ReactJS", "Express", "Typescript"];
-  const rightLists = ["VueJS", "NuxtJS", "GraphQL"];
+  const leftLists = ["", "Express", "ReactJS"];
+  const rightLists = ["Mongoose", "NodeJS", ""];
 
   const [copied, setCopied] = useState(false);
 
@@ -67,7 +67,7 @@ export const BentoGridItem = ({
   };
 
   const handleCopy = () => {
-    const text = "hsu@jsmastery.pro";
+    const text = "surajsomani2002@gmail.com";
     navigator.clipboard.writeText(text);
     setCopied(true);
   };
